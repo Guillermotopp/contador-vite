fb5d519f497e5671b33d13c97f9648dc82df4d02
guillermotoppph <guillermotoppph@gmail.com>
2024-07-10 16:18:06 -0300

    Contador como componente de React que se incrementa cada segundo. Construido con Vite, React y Javascript V.1.0 Funcional

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Temporizador from "./componentes/Temporizador";
+
+const App = () => {
+    return (
+        <div>
+            <Temporizador />
+        </div>
+    );
+};
+
+export default App;
